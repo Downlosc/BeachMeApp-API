1533cc1ecac349a15a8075821b173965a96131eb
Oscar Benedetti <oscarbene@hotmail.it>
2016-12-19 13:26:56 +0100

    Added crud wristband

--- server/models/wristband.js
+++ server/models/wristband.js
@@ -1,7 +1,99 @@
 var connection = require('../db/connection');
 
-function wristBand(){
+function wristBand() {
+    this.getAll = function(res) {
+        connection.acquire(function(err, con) {
+            con.query('SELECT * FROM wristband', function(err, result) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "message": "Get all wristband failed";
+                    });
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        });
+    };
 
+    this.getOne = function(id, res) {
+        connection.acquire(function(err, con) {
+            con.query('SELECT * FROM wristband WHERE id = ?', [id], function(err, result) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "message": "Get wristband with id " + id + "failed : " + err;
+                    });
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        });
+    };
+
+    this.create = function(wristband, res) {
+        connection.acquire(function(err, con) {
+            con.query('INSERT INTO wristband SET ?', wristband, function(err, result) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "message": "Creation of the wristband failed";
+                    });
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        });
+    };
+
+    this.update = function(id, res) {
+        connection.acquire(function(err, con) {
+            con.query('UPDATE wristband SET ? WHERE id = ?', [wristband, id], function(err, result) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "messgae": "Updating wristband with id" + id + "failed" + err;
+                    });
+                } else {
+                    res.status(200).json(result);
+                }
+            });
+        });
+    };
+
+    this.delete = function(id, res) {
+        connection.acquire(function(err, con) {
+            con.query('DELETE FROM wristband WHERE id = ?', [id], function(err, result) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "message": "Deleting wristband with id " + id + "failed " + err;
+                    });
+                } else {
+                    res.staus(200).json(result);
+                }
+            });
+        });
+    };
+
+    this.initTable = function(req, res) {
+        connection.acquire(function(err, con) {
+            con.query([
+                'CREATE TABLE IF NOT EXITS `wristband`(',
+                'id BIGINT(20) NOT NULL AUTO_INCREMENT,',
+                'PRIMARY KEY (`id`)',
+                ')ENGINE=InnoDB CHARSET=utf8'
+            ].join(''), function(err, result) {
+                if (err) throw err;
+                if (!result.warningCount) {
+                    console.log('Table WRISTBAND created with success');
+                }
+
+                con.release();
+                return true;
+            });
+        });
+    }
 }
 
 module.exports = new wristBand();
